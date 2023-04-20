ec2dd54195466b81579475821b9f16945f0babc2
Carlos Agamez <carlos.agamez@sigma3ds.com>
2023-04-20 05:36:37 -0500

    actualizacion de plantillas

--- retos_admin/urls.py
+++ retos_admin/urls.py
@@ -5,7 +5,30 @@ from . import views
 app_name="retos_admin"
 
 urlpatterns = [
-    path('', views.index, name="index"),
+        # login
     path('login/', views.login_admin, name="login"),
-    path('create_retos/', views.create_retos, name="create_retos")
+        # retos
+    path('', views.index, name="index"),
+    path('create_retos/', views.create_retos, name="create_retos"),
+    path('retos_detail/', views.retos_detail, name="retos_detail"),
+
+        # ----------- comunidades -------------------------
+    path('comunidades/', views.comunidades, name="comunidades"),
+    path('create_comunidades/', views.create_comunidades, name="create_comunidades"),
+    path('comunidades_detail/', views.comunidades_detail, name="comunidades_detail"),
+
+    # ----------- Instituciones -----------------------------------
+    path('instituciones/', views.instituciones, name="instituciones"),
+    path('create_instituciones/', views.create_instituciones, name="create_instituciones"),
+    path('instituciones_detail/', views.instituciones_detail, name="instituciones_detail"),
+
+    # ----------- espacios de co creacion -------------------------
+    path('esp_co_creacion/', views.esp_co_creacion, name="esp_co_creacion"),
+    path('create_esp_co_creacion/', views.create_esp_co_creacion, name="create_esp_co_creacion"),
+    path('esp_co_creacion_detail/', views.esp_co_creacion_detail, name="esp_co_creacion_detail"),
+
+    # ----------- Participantes -------------------------------------
+    path('participantes/', views.participantes, name="participantes"),
+    path('create_participantes/', views.create_participantes, name="create_participantes"),
+    path('participantes_detail/', views.participantes_detail, name="participantes_detail"),
 ]
