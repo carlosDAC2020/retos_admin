d14742a1cdbf0d4ca2b859ab9b38e09d6348b71c
Carlos Agamez <carlos.agamez@sigma3ds.com>
2023-04-20 05:10:44 -0500

    estilso de listas y formulariod de creacion de las seccionde de retos , comunidades, instituciones , espacios de co-creacion y participantes

--- retos_admin/views.py
+++ retos_admin/views.py
@@ -17,14 +17,82 @@ def login_admin(request):
         return render(request, 'retos_admin/login.html')
 
 
-
+# -------------- Retos ------------------------------------
 def index(request):
-    return render(request, "retos_admin/retos.html",{
-        "seccion":"Lista de Retos"
+    print("retos")
+    return render(request, "retos_admin/retos/retos.html",{
+        "retos":range(1,11)
     })
 
 def create_retos(request):
     if request.method == 'POST':
-        return HttpResponse("reto creado")
+        return redirect('retos_admin:retos_detail')
     else:
-        return render(request, 'retos_admin/create_retos.html')
+        return render(request, 'retos_admin/retos/create.html')
+
+def retos_detail(request):
+    return render(request, 'retos_admin/retos/detail.html')
+
+    # ----------- comunidades -------------------------
+def comunidades(request):
+    print("comunidades")
+    return render(request, 'retos_admin/comunidades/comunidades.html',{
+        "comunidades":range(1,15)
+    })
+
+def create_comunidades(request):
+    if request.method == 'POST':
+        return redirect('retos_admin:comunidades_detail')
+    else:
+        return render(request, 'retos_admin/comunidades/create.html')
+
+def comunidades_detail(request):
+    return render(request, 'retos_admin/comiunidades/detail.html')
+
+    # ----------- Instituciones -----------------------------------
+def instituciones(request):
+    print("instituciones")
+    return render(request, "retos_admin/instituciones/instituciones.html",{
+        "instituciones":range(1,11)
+    })
+
+def create_instituciones(request):
+    if request.method == 'POST':
+        return redirect('retos_admin:instituciones_detail')
+    else:
+        return render(request, 'retos_admin/instituciones/create.html')
+
+def instituciones_detail(request):
+    return render(request, 'retos_admin/instituciones/detail.html')
+
+    # ----------- espacios de co creacion -------------------------
+def esp_co_creacion(request):
+    print("espacios de cocreacion")
+    return render(request, "retos_admin/esp_co_creacion/esp_co_creacion.html",{
+        "esp_co_creacion":range(1,11)
+    })
+
+def create_esp_co_creacion(request):
+    if request.method == 'POST':
+        return redirect('retos_admin:esp_co_creacion_detail')
+    else:
+        return render(request, 'retos_admin/esp_co_creacion/create.html')
+
+def esp_co_creacion_detail(request):
+    return render(request, 'retos_admin/esp_co_creacion/detail.html')
+
+    # ----------- Participantes -------------------------------------
+def participantes(request):
+    print("participantes")
+    return render(request, "retos_admin/participantes/participantes.html",{
+        "participantes":range(1,11)
+    })
+
+def create_participantes(request):
+    if request.method == 'POST':
+        return redirect('retos_admin:participantes_detail')
+    else:
+        return render(request, 'retos_admin/participantes/create.html')
+
+def participantes_detail(request):
+    return render(request, 'retos_admin/participantes/detail.html')
